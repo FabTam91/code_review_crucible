c8adab2e875eb1e9be23d1faf50e91b7705f3b14
FabTam91 <fabiantamas91@gmail.com>
2025-05-01 13:09:40 +0200

    Update fibonacci.py in accordance to the review

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,6 @@
 def fibonacci(n):
-    numbers = []
-    first_number = 0
-    second_number = 1
-    numbers.append(first_number)
-    numbers.append(second_number)
-    for k in range(2 , n) :
+    numbers = [0, 1]
+    for _ in range(2, n):
         new_number = numbers[-1] + numbers[-2]
         numbers.append(new_number)
     for number in numbers:
